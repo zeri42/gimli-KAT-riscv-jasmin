58e45f3467a9129b48ae473d352a8fb3c5a837c3
Julian Wälde <julian.waelde@hs-rm.de>
2024-04-16 18:04:56 +0200

    Add gimli reference impl and now the KATs fail with the jasmin version but not the reference version

--- Makefile
+++ Makefile
@@ -3,11 +3,14 @@
 CC=riscv32-none-elf-gcc
 CFLAGS= -g -ffreestanding -Os 
 LDFLAGS= -Wl,--gc-sections -nostartfiles -nostdlib -nodefaultlibs -Wl,-T,linker.ld
+
+gimli_KAT.h:
+	python parse_KAT.py  > gimli_KAT.h
 gimli.S: 
 	jasminc -arch risc-v jasmin/compiler/tests/success/risc-v/gimli.jazz -o gimli.S
 
-main.elf: main.o crt.o gimli.o linker.ld
-	$(CC) $(CFLAGS) $(LDFLAGS) main.o crt.o gimli.o -o main.elf
+main.elf: gimli_KAT.h main.o crt.o gimli.o
+	$(CC) $(CFLAGS) $(LDFLAGS) -o main.elf $^
 
 
 sim: main.elf
@@ -15,4 +18,7 @@ sim: main.elf
 	qemu-system-riscv32 -machine virt -bios none -kernel main.elf -nographic
 all: sim
 clean: 
-	rm main.elf main.o crt.o gimli.o
+	rm main.elf
+	rm main.o 
+	rm crt.o
+	rm gimli.o ref_gimli.o gimli_KAT.h
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "gimli_KAT.h"
 void putc_(char c) {
 	*(char *)(0x10000000) = c;
 }
@@ -7,6 +8,44 @@ void puts_(char *c) {
 	while (b=*c++)
 		putc_(b);
 }
+
+void cpy(unsigned int *dst, unsigned int *src, unsigned int cnt) {
+	for (int i = 0; i < cnt; i++) {
+		dst[i] = src[i];
+	}
+}
+
+int cmp(unsigned int *a, unsigned int *b, unsigned int cnt) {
+	int ret = 0;
+	for (int i = 0; i < cnt; i++) {
+		ret += a[i]^b[i];
+	}
+	return ret;
+}
+unsigned int *gimli(unsigned int *);
+
+
 void main() {
+	int ret=0; 
+	unsigned int buf [48];
 	puts_("Hello World\n");
+
+	for (int i = 0; i < 100; i++) {
+		unsigned int *in = (unsigned int*)(gimli_KAT+2*i*48);
+		cpy(buf,in,48);
+		gimli(buf);
+		if(ret|=cmp(buf,in+12,12)) {
+			puts_("-");
+		} else {
+			puts_(".");
+		}
+
+	}
+
+	if (ret == 0) {
+		puts_("SUCCESS");
+	} else {
+		puts_("FAIL!");
+	}
+	puts_("\n");
 }
